6b5c2c4b38eef75b3e2cae5b55ab5724ed2aabef
NichHarris <harris.nicholas1998@gmail.com>
2022-10-14 11:20:33 -0400

    Update readme

---
+++ README.md
@@ -0,0 +1,65 @@
+Repository Link: https://github.com/nickawwas/reddit_emotion_classification
+
+COMP 472 - Mini-Project 1
+
+# Setup guide:
+
+## Setting up virtual environment
+Ensure you Python version is either 3.7 or 3.8 and 64-bit
+
+### For macOS/linux:
+
+```
+pip3 install venv
+python -m venv comp472
+source comp472/bin/activate
+pip3 install -r requirements.txt
+```
+
+### For Windows:
+
+```
+pip install venv
+python -m venv comp472
+comp472/Scripts/activate
+pip install -r requirements.txt
+```
+
+## Running program to create models
+(Note this will take several hours to days to run)
+Run the command below:
+```
+python main.py
+```
+
+This will create the charts associated with the dataset (barchart and piechart)
+
+A folder called `models` will be created, inside each of the classification methods 
+will have their models stored, for both emotions and sentiments. These can be used
+later to load, avoiding the need to rebuild the models
+
+
+## Running program with prebuilt models
+
+Run the command below:
+```
+python pre-built {model_name} {type}
+```
+
+List of entries for `model_name`:
+- decision_tree
+- naive_bayes
+- perceptron
+- GridSearch_DCT
+- GridSearch_MNB
+- GridSearch_Perceptron
+
+List of entries for `type`:
+- emotions
+- sentiments
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -81,11 +81,13 @@ def decision_tree_classifier(comments, feature):
     return clf
 
 def perceptron_classifier(comments, feature):
-    clf = MLPClassifier(random_state=1, max_iter=10)
+    # TODO: Remove max iter, default = 200
+    clf = MLPClassifier(max_iter=10)
     clf.fit(comments, feature)
     return clf
 
 def top_mnb_classifier(comments, feature):
+    # n_jobs param value -1: allows utilization of maximum processors
     clf = GridSearchCV(MultinomialNB(), param_grid={'alpha': [0.01, 0.1, 0.5, 1.0]}, n_jobs=-1)
     clf.fit(comments, feature)
     return clf
@@ -101,24 +103,22 @@ def top_decision_tree_classifier(comments, feature):
     return clf
 
 def top_perceptron_classifier(comments, feature):
+    # TODO: Remove max iter, default = 200
     params = {
         'solver': ['adam', 'sgd'],
         'activation': ['softmax', 'tanh', 'relu', 'identity'],
         'hidden_layer_sizes': [(30, 30, 30), (10, 30, 50)],
-        'max_iter': [5],
-        'random_state': [1]
+        'max_iter': [5]
     }   
     clf = GridSearchCV(MLPClassifier(), param_grid=params, n_jobs=-1)
     clf.fit(comments, feature)
     return clf
 
-def report_results(clf, classifier_type: str, feature_type: str, comments, feature):
+def report_results(clf, classifier_type: str, feature_type: str, comments, feature, is_gs: bool):
     prediction = clf.predict(comments)
     score = clf.score(comments, feature)
 
-    print(f'{classifier_type} {feature_type} Prediction: {prediction}')
-    print(f'{classifier_type} {feature_type} Test: {feature}')
-    print(f'Score: {score}')
+    print(f'{classifier_type} {feature_type} Score: {score}')
 
     # create performance file entry
     with open('performance.txt', 'a') as performance:
@@ -127,6 +127,10 @@ def report_results(clf, classifier_type: str, feature_type: str, comments, featu
         performance.writelines(np.array2string(confusion_matrix(feature, prediction)))        
         performance.writelines('\n\nClassification Report:\n')
         performance.writelines(f'{classification_report(feature, prediction, zero_division=1)}\n\n')
+        
+        if is_gs:
+            performance.writelines(f'Best parameters: {clf.best_params_}\n\n')
+            performance.writelines(f'Best score: {clf.best_score_}\n\n')
 
 def export_model(clf, classifier_type: str, feature_type: str):
     if not os.path.exists(f'models/{classifier_type}'):
@@ -167,43 +171,43 @@ if __name__ == '__main__':
     print('Multinominal Naive Bayes Classification For Emotions')
     with Spinner():
         mnb_emotions = naive_bayes_classifier(comments_train, emotions_train)
-    report_results(mnb_emotions, 'Naive Bayes Classifier', 'Emotions', comments_test, emotions_test)
+    report_results(mnb_emotions, 'Naive Bayes Classifier', 'Emotions', comments_test, emotions_test, False)
     export_model(mnb_emotions, NAIVE_BAYES, 'emotions')
 
     print('Multinominal Naive Bayes Classification For Sentiments')
     with Spinner():
         mnb_sentiments = naive_bayes_classifier(comments_train, sentiments_train)
-    report_results(mnb_sentiments, 'Naive Bayes Classifier', 'Sentiments', comments_test, sentiments_test)
+    report_results(mnb_sentiments, 'Naive Bayes Classifier', 'Sentiments', comments_test, sentiments_test, False)
     export_model(mnb_sentiments, NAIVE_BAYES, 'sentiments')
 
     print('Decision Tree classification For Emotions')
     with Spinner():
         dct_emotions = decision_tree_classifier(comments_train, emotions_train)
-    report_results(dct_emotions, 'Decision Tree Classifier', 'Emotions', comments_test, emotions_test)
+    report_results(dct_emotions, 'Decision Tree Classifier', 'Emotions', comments_test, emotions_test, False)
     export_model(dct_emotions, DECISION_TREE, 'emotions')
 
     print('Decision Tree classification For Sentiments')
     with Spinner():
         dct_sentiments = decision_tree_classifier(comments_train, sentiments_train)
-    report_results(dct_sentiments, 'Decision Tree Classifier', 'Sentiments', comments_test, sentiments_test)
+    report_results(dct_sentiments, 'Decision Tree Classifier', 'Sentiments', comments_test, sentiments_test, False)
     export_model(dct_sentiments, DECISION_TREE, 'sentiments')
 
     print('Perceptron classification For Emotions')
     with Spinner():
         mlp_emotions = perceptron_classifier(comments_train, emotions_train)
-    report_results(mlp_emotions, 'Perceptron', 'Emotions', comments_test, emotions_test)    
+    report_results(mlp_emotions, 'Perceptron', 'Emotions', comments_test, emotions_test, False)    
     export_model(mlp_emotions, PERCEPTRON, 'emotions')
 
     print('Perceptron classification For Sentiments')
     with Spinner():
         mlp_sentiments = perceptron_classifier(comments_train, sentiments_train)
-    report_results(mlp_sentiments, 'Perceptron', 'Sentiments', comments_test, sentiments_test)
+    report_results(mlp_sentiments, 'Perceptron', 'Sentiments', comments_test, sentiments_test, False)
     export_model(mlp_sentiments, PERCEPTRON, 'sentiments')
 
     print('GridSearch Multinominal Naive Bayes Classification For Emotions')
     with Spinner():
         gs_mnb_emotions = top_mnb_classifier(comments_train, emotions_train)
-    report_results(gs_mnb_emotions, 'GridSearch_MNB', 'Emotions', comments_test, emotions_test)
+    report_results(gs_mnb_emotions, 'GridSearch_MNB', 'Emotions', comments_test, emotions_test, True)
     export_model(gs_mnb_emotions, 'GridSearch_MNB', 'emotions')
     print(gs_mnb_emotions.best_params_)
     print(gs_mnb_emotions.best_score_)
@@ -211,7 +215,7 @@ if __name__ == '__main__':
     print('GridSearch Multinominal Naive Bayes Classification For Sentiments')
     with Spinner():
         gs_mnb_sentiments = top_mnb_classifier(comments_train, sentiments_train)
-    report_results(gs_mnb_sentiments, 'GridSearch_MNB', 'Sentiments', comments_test, sentiments_test)
+    report_results(gs_mnb_sentiments, 'GridSearch_MNB', 'Sentiments', comments_test, sentiments_test, True)
     export_model(gs_mnb_sentiments, 'GridSearch_MNB', 'sentiments')
     print(gs_mnb_sentiments.best_params_)
     print(gs_mnb_sentiments.best_score_)
@@ -219,7 +223,7 @@ if __name__ == '__main__':
     print('GridSearch Decision Tree classification For Emotions')
     with Spinner():
         gs_dct_emotions = top_decision_tree_classifier(comments_train, emotions_train)
-    report_results(gs_dct_emotions, 'GridSearch_DCT', 'Emotions', comments_test, emotions_test)
+    report_results(gs_dct_emotions, 'GridSearch_DCT', 'Emotions', comments_test, emotions_test, True)
     export_model(gs_dct_emotions, 'GridSearch_DCT', 'emotions')
     print(gs_dct_emotions.best_params_)
     print(gs_dct_emotions.best_score_)
@@ -227,7 +231,7 @@ if __name__ == '__main__':
     print('GridSearch Decision Tree classification For Sentiments')
     with Spinner():
         gs_dct_sentiments = top_decision_tree_classifier(comments_train, sentiments_train)
-    report_results(gs_dct_sentiments, 'GridSearch_DCT', 'Sentiments', comments_test, sentiments_test)
+    report_results(gs_dct_sentiments, 'GridSearch_DCT', 'Sentiments', comments_test, sentiments_test, True)
     export_model(gs_dct_sentiments, 'GridSearch_DCT', 'sentiments')
     print(gs_dct_sentiments.best_params_)
     print(gs_dct_sentiments.best_score_)
@@ -235,7 +239,7 @@ if __name__ == '__main__':
     print('GridSearch Perceptron classification For Emotions')
     with Spinner():
         gs_prceptron_emotions = top_perceptron_classifier(comments_train, emotions_train)
-    report_results(gs_prceptron_emotions, 'GridSearch_Perceptron', 'Emotions', comments_test, emotions_test)
+    report_results(gs_prceptron_emotions, 'GridSearch_Perceptron', 'Emotions', comments_test, emotions_test, True)
     export_model(gs_prceptron_emotions, 'GridSearch_Perceptron', 'emotions')
     print(gs_prceptron_emotions.best_params_)
     print(gs_prceptron_emotions.best_score_)
@@ -243,7 +247,7 @@ if __name__ == '__main__':
     print('GridSearch Perceptron classification For Sentiments')
     with Spinner():
         gs_prceptron_sentiments = top_perceptron_classifier(comments_train, sentiments_train)
-    report_results(gs_prceptron_sentiments, 'GridSearch_Perceptron', 'Sentiments', comments_test, sentiments_test)
+    report_results(gs_prceptron_sentiments, 'GridSearch_Perceptron', 'Sentiments', comments_test, sentiments_test, True)
     export_model(gs_prceptron_sentiments, 'GridSearch_Perceptron', 'sentiments')
     print(gs_prceptron_sentiments.best_params_)
     print(gs_prceptron_sentiments.best_score_)
